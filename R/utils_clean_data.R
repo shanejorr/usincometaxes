fb21875f3e8b73fc583b88302db5a65f5ce713f6
Thomas Wells <thomascwells@gmail.com>
2022-05-25 18:08:31 -0600

    Fix formatting

--- R/utils_clean_data.R
+++ R/utils_clean_data.R
@@ -68,15 +68,15 @@ taxsim_cols <- function() {
 
   # NOTE: You need to change replace_missing() and check_required_cols() if you change this function.
 
-c(
-  'taxsimid', 'year', 'mstat', # required
-  'state', 'page', 'sage',
-  'depx', 'age1', 'age2', 'age3', # dependents
-  'pwages', 'swages', 'psemp', 'ssemp', 'dividends', 'intrec', 'stcg', 'ltcg', 'otherprop', 'nonprop',
-  'pensions', 'gssi', 'pui', 'sui', 'transfers', 'rentpaid', 'proptax', 'otheritem',
-  'childcare', 'mortgage', 'scorp', 'pbusinc', 'pprofinc', 'sbusinc', 'sprofinc',
-  'mtr', 'idtl'
-)
+  c(
+    'taxsimid', 'year', 'mstat', # required
+    'state', 'page', 'sage',
+    'depx', 'age1', 'age2', 'age3', # dependents
+    'pwages', 'swages', 'psemp', 'ssemp', 'dividends', 'intrec', 'stcg', 'ltcg', 'otherprop', 'nonprop',
+    'pensions', 'gssi', 'pui', 'sui', 'transfers', 'rentpaid', 'proptax', 'otheritem',
+    'childcare', 'mortgage', 'scorp', 'pbusinc', 'pprofinc', 'sbusinc', 'sprofinc',
+    'mtr', 'idtl'
+  )
 
 }
 
